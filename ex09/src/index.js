92e332113db136833a446e3842804acffcc16238
KatarinaRS <katarina.roser@gmail.com>
2022-04-17 22:22:23 +0200

    Find a user by Id

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,67 +1,58 @@
-// Lodash
-const _ = require('lodash');
-
-// Only change code below this line
-// users nested array with four objects starts here
 var users = [
     {
         id: 1,
-        firstName: 'John',
-        lastName: 'Doe',
+        firstName: "John",
+        lastName: "Doe",
         age: 24,
-        gender: 'male'
+        gender: "male"
     },
     {
         id: 2,
-        firstName: 'Jane',
-        lastName: 'Doe',
+        firstName: "Jane",
+        lastName: "Doe",
         age: 5,
-        gender: 'female'
+        gender: "female"
     },
     {
         id: 3,
-        firstName: 'Jim',
-        lastName: 'Carrey',
+        firstName: "Jim",
+        lastName: "Carrey",
         age: 54,
-        gender: 'male'
+        gender: "male"
     },
     {
         id: 4,
-        firstName: 'Kate',
-        lastName: 'Winslet',
+        firstName: "Kate",
+        lastName: "Winslet",
         age: 40,
-        gender: 'female'
+        gender: "female"
     }
-];
-
-// users nested array with four objects ends here
-
-// getUsers function - list of users starts here
-const getUsers = () => {
-    var output ='';
-    for (let i=0; i<users.length; i++) {
-        output += `${users[i].id} - ${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}\n`;
+  ];
+  
+  function getUsers() {
+    var output = "";
+    for (let i = 0; i < users.length; i++) {
+        output += `${users[i].id} - ${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender} \n`;
     }
     return output;
-}
-// getUsers function - list of users ends here
-// findUser(id) function starts here
-
-const findUserById = (id) => {
+  
+  }
+  function findUserById(id) {
     try {
-        var user = _.find(users, { id : id });
-        var iFindUser = `${user.id} - ${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
+        var user = users.find(x => x.id === id);
+        var iFindUser = ` ${user.id}- ${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
+  
+        console.log(iFindUser);
         return iFindUser;
-    } catch(error) {
-        console.log("Cannot read property 'id'");
-        return "Cannot read property 'id'";
+  
+  
+    } catch (error) {
+  
+        console.log("Cannot read property 'id' ");
     }
-}
-
-
-// findUser(id) function ends here
-    // Only change code above this line
-getUsers();
-findUserById(6); // Change this line
-
-module.exports = findUserById;
+  }
+  
+  console.log(getUsers());
+  findUserById(1);
+  
+  module.exports = findUserById;
